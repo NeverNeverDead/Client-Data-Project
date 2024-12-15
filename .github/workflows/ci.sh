dbeeb0feb5e8f1ad37e3bf32d9f625655fbf7875
Oleg Dolgov <oleg.dolgov.2014@mail.ru>
2024-12-15 19:32:44 +0300

    version 7

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build Flask API image
-docker build -t client_data_project-flask_api:latest .
+docker build -t client_data_project-flask_api:latest backend/
 
 # Tag the Flask API image
 docker tag client_data_project-flask_api:latest curvereality/client_data_project-flask_api:latest
@@ -10,7 +10,7 @@ docker tag client_data_project-flask_api:latest curvereality/client_data_project
 docker push curvereality/client_data_project-flask_api:latest
 
 # Build Frontend image
-docker build -t client_data_project-frontend:latest -f frontend/Dockerfile .
+docker build -t client_data_project-frontend:latest frontend/
 
 # Tag the Frontend image
 docker tag client_data_project-frontend:latest curvereality/client_data_project-frontend:latest
